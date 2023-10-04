2e8f62eb9d60b7fe5ef5292a204a47bf1bf9bc04
nicolascaliari <nicolascaliari28@gmail.com>
2023-10-04 15:57:13 -0300

    added front and back

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+const {Server } = require('socket.io');
+
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+
+io.on('connection', (socket) => {
+    console.log("Client connected");
+
+    socket.on('message', (body) => {
+        socket.broadcast.emit('message', {
+            body,
+            from:socket.id.slice(6)
+        })
+    })
+
+
+})
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
