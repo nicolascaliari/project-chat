3400ede1bd367ce06fff442e610f0c5e010983aa
nicolascaliari <nicolascaliari28@gmail.com>
2023-10-07 11:43:59 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
 
-function App() {
+const socket = io("http://localhost:4000");
+// const socket = io();
+
+export default function App() {
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    socket.on("message", receiveMessage)
+
+    return () => {
+      socket.off("message", receiveMessage);
+    };
+  }, []);
+
+  const receiveMessage = (message) =>
+    setMessages(state => [message, ...state]);
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newMessage = {
+      body: message,
+      from: "Me",
+    };
+    setMessages(state => [newMessage, ...state]);
+    setMessage("");
+    socket.emit("message", newMessage.body);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen bg-zinc-800 text-white flex items-center justify-center">
+      <form onSubmit={handleSubmit} className="bg-zinc-900 p-10">
+        <h1 className="text-2xl font-bold my-2">Chat React</h1>
+        <input
+          name="message"
+          type="text"
+          placeholder="Write your message..."
+          onChange={(e) => setMessage(e.target.value)}
+          className="border-2 border-zinc-500 p-2 w-full text-black"
+          value={message}
+          autoFocus
+        />
+
+        <ul className="h-80 overflow-y-auto">
+          {messages.map((message, index) => (
+            <li
+              key={index}
+              className={`my-2 p-2 table text-sm rounded-md ${message.from === "Me" ? "bg-sky-700 ml-auto" : "bg-black"
+                }`}
+            >
+              <b>{message.from}</b>:{message.body}
+            </li>
+          ))}
+        </ul>
+      </form>
     </div>
   );
 }
-
-export default App;
